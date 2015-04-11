3f80f6e82860974c8c752005be5cfe3fcfb2018c
ADarkHero <maik_riedlsperger@yahoo.de>
2015-04-11 21:14:56 +0200

    Draggable Help/Config

--- src/quicktweethero/cnf/Config.java
+++ src/quicktweethero/cnf/Config.java
@@ -1,17 +1,22 @@
 package quicktweethero.cnf;
 
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Config {
+    
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public Config() throws IOException{
-        Stage newStage = new Stage();
+        final Stage newStage = new Stage();
         newStage.initModality(Modality.APPLICATION_MODAL);
         
         Parent root = FXMLLoader.load(getClass().getResource("Config.fxml"));
@@ -20,6 +25,22 @@ public class Config {
         newStage.setScene(newScene);
         newStage.setTitle("Configuration");
         newStage.initStyle(StageStyle.UNDECORATED);
+        
+        //Make draggable
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                newStage.setX(event.getScreenX() - xOffset);
+                newStage.setY(event.getScreenY() - yOffset);
+            }
+        });
         
         newStage.show();
     }
--- src/quicktweethero/help/Help.java
+++ src/quicktweethero/help/Help.java
@@ -1,9 +1,11 @@
 package quicktweethero.help;
 
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -11,8 +13,11 @@ import javax.swing.JOptionPane;
 
 public class Help {
     
+    private double xOffset = 0;
+    private double yOffset = 0;
+    
     public Help() throws IOException{
-        Stage newStage = new Stage();
+        final Stage newStage = new Stage();
         newStage.initModality(Modality.APPLICATION_MODAL);
         
         Parent root = FXMLLoader.load(getClass().getResource("Help.fxml"));
@@ -21,6 +26,22 @@ public class Help {
         newStage.setScene(newScene);
         newStage.setTitle("Help");
         newStage.initStyle(StageStyle.UNDECORATED);
+        
+        //Make draggable
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                newStage.setX(event.getScreenX() - xOffset);
+                newStage.setY(event.getScreenY() - yOffset);
+            }
+        });
         
         newStage.show();
     }
